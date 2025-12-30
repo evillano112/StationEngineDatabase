946550942e8af3779fe20a3019cd62b003d97d07
Edward Villano <132305649+evillano112@users.noreply.github.com>
2025-12-30 11:41:33 -0500

    Add comment for Python script database access
    Added comment to indicate usage by Python scripts.

--- CreateDB.sql
+++ CreateDB.sql
@@ -4,8 +4,10 @@ CREATE DATABASE radio_db
 
 CREATE USER 'radio_user'@'localhost'
 IDENTIFIED BY 'password';
+-- used by python scripts to access db
 
 GRANT ALL PRIVILEGES ON radio_db.* TO 'radio_user'@'localhost';
 FLUSH PRIVILEGES;
 
 USE radio_db;
+
